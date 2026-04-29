cef7f47db9f1ad739c6e537744f64fdd9dbb8f6a
onkarpethe15-ai <omkarpethe4@gmail.com>
2026-04-29 16:03:55 +0530

    added E2E Scenarion for ticket booking application

--- tests/EventBook.spec.js
+++ tests/EventBook.spec.js
@@ -1,12 +1,16 @@
 import { test, expect } from "@playwright/test";
 
 test("Evenet booking test", async ({ page }) => {
-  await page.goto("https://eventhub.rahulshettyacademy.com");
-  await page.getByPlaceholder("you@email.com").fill("mailtonkar@gmail.com");
-  await page.getByLabel("password").fill("Panda@2025");
-  await page.locator("#login-btn").click();
+  async function login(page) {
+    await page.goto("https://eventhub.rahulshettyacademy.com");
+    await page.getByPlaceholder("you@email.com").fill("mailtonkar@gmail.com");
+    await page.getByLabel("password").fill("Panda@2025");
+    await page.locator("#login-btn").click();
+  }
+  await login(page);
+
   const link_text = page.getByText("Browse Events →");
-  expect(await link_text).toBeVisible();
+  await expect(await link_text).toBeVisible();
   await page.getByRole("button", { name: "Admin" }).click();
   await page.locator("//a[text()='Manage Events']").nth(0).click();
   const title = `pinno ki tuk tuk ${Date.now()}`;
@@ -18,6 +22,65 @@ test("Evenet booking test", async ({ page }) => {
   await page.locator("input[id='price-($)']").fill("20");
   await page.locator("input[id='total-seats']").fill("700");
   await page.locator("#add-event-btn").click();
-  expect(await page.getByText("Event created!")).toBeVisible();
+  await expect(page.getByText("Event created!")).toBeVisible();
+
   await page.getByTestId("nav-events").click();
+  const event_cards = page.getByTestId("event-card");
+  await expect(await event_cards.first()).toBeVisible();
+  await expect(page.getByText(title)).toBeVisible({ timeout: 5000 });
+
+  const sit_list = await page
+    .locator(
+      `//h3[text()="${title}"]/parent::a/following-sibling::div[2]/div/span`,
+    )
+    .textContent();
+
+  const sitcount = Number(sit_list.split(" ")[0]);
+  console.log(sitcount);
+
+  await page
+    .locator(`//h3[text()='${title}']/parent::a/following-sibling::div[2]/a`)
+    .click();
+
+  expect(await page.locator("#ticket-count").textContent()).toMatch("1");
+  await page.getByLabel("Full Name").fill("jhon philips");
+  await page.locator("#customer-email").fill("philips@15gmail.com");
+  await page.getByPlaceholder("+91 98765 43210").fill("9325711274");
+  await page.locator(".confirm-booking-btn").click();
+  const bookingref = page.locator(".booking-ref").first();
+  await expect(bookingref).toBeVisible();
+  const bookingreno = await bookingref.textContent();
+  const trim_bookrefno = bookingreno.trim();
+  console.log(trim_bookrefno);
+  await page.locator("#nav-bookings").click();
+  await expect(page).toHaveURL(
+    "https://eventhub.rahulshettyacademy.com/bookings",
+  );
+  const booking_card = page.locator("#booking-card").first();
+  await expect(booking_card).toBeVisible();
+
+  await expect(
+    await page.locator(".booking-ref").filter({ hasText: trim_bookrefno }),
+  ).toBeVisible();
+
+  await expect(
+    await page
+      .locator(
+        `//span[text()='${trim_bookrefno}']/parent::div/following-sibling::h3`,
+      )
+      .textContent(),
+  ).toMatch(title);
+
+  await page.locator("//a[text()='Events']").click();
+
+  const event_cards_t = page.getByTestId("event-card").first();
+  await expect(event_cards_t).toBeVisible();
+  await expect(page.getByText(title)).toBeVisible({ timeout: 5000 });
+  const sit_list_t = await page
+    .locator(
+      `//h3[text()="${title}"]/parent::a/following-sibling::div[2]/div/span`,
+    )
+    .textContent();
+  const sitcount_two = await Number(sit_list_t.split(" ")[0]);
+  await expect(sitcount_two.toString()).toMatch((sitcount - 1).toString());
 });
