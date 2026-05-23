8c6368ef55179b293e4bc828474fa329a0d3ac53
onkarpethe15-ai <omkarpethe4@gmail.com>
2026-05-23 18:54:48 +0530

    latest merge

--- playwright.service.config.js
+++ playwright.service.config.js
@@ -1,17 +1,18 @@
 const { defineConfig } = require("@playwright/test");
 const { createAzurePlaywrightConfig, ServiceOS } = require("@azure/playwright");
 const { DefaultAzureCredential } = require("@azure/identity");
+
 const config = require("./playwright.config");
 
-module.exports = defineConfig(
-  config,
-  createAzurePlaywrightConfig(config, {
+module.exports = defineConfig({
+  ...config,
+
+  reporter: [["html", { open: "never" }], ["@azure/playwright/reporter"]],
+
+  ...createAzurePlaywrightConfig(config, {
     exposeNetwork: "<loopback>",
     connectTimeout: 3 * 60 * 1000,
     os: ServiceOS.LINUX,
     credential: new DefaultAzureCredential(),
   }),
-  {
-    reporter: [["html", { open: "never" }], ["@azure/playwright/reporter"]],
-  },
-);
+});
